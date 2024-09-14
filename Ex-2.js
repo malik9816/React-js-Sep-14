de4025614b345f200f856282e2df0bc00066c716
malik9816 <malikdudekula05@gmail.com>
2024-09-14 21:08:37 +0530

    Ex-2 is modify

---
+++ Ex-2.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Ex-2 = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Ex-2.propTypes = {
+
+}
+
+export default Ex-2
+
